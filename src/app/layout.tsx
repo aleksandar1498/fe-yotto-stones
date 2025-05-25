c210055abeac5527f7e123d8819d01fc6ea63308
AleksandarStefanov1498 <aleksandar.fs@gmail.com>
2025-05-25 11:05:56 +0300

    fix icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,14 +28,14 @@ export const metadata = {
   metadataBase: new URL("https://www.yottostones-bg.com"),
   icons: {
     icon: [
-      { url: "/favicon.ico" },
+      { url: "/favicon.svg" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
-    shortcut: "/favicon.ico",
+    shortcut: "/favicon.svg",
   },
   manifest: "/site.webmanifest",
   other: {
